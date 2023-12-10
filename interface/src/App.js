7e42e31d0931ae0ab8acce1a1883bcb81ed82bf3
cpp-phoenix <gurjinders92571@gmail.com>
2023-12-10 07:50:19 +0530

    deployments to multichain done

--- interface/src/App.js
+++ interface/src/App.js
@@ -8,12 +8,13 @@ import {
 } from '@rainbow-me/rainbowkit';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  base,
-  zora,
+  arbitrumGoerli,
+  filecoinCalibration,
+  scrollSepolia,
+  celoAlfajores,
+  baseGoerli,
+  mantleTestnet,
+  polygonZkEvmTestnet
 } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -42,8 +43,54 @@ export const polygonMumbai = {
   },
 } 
 
+export const okxX1 = {
+  id: 195,
+  name: 'X1 testnet',
+  network: 'X1 testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'OKB',
+    symbol: 'OKB',
+  },
+  rpcUrls: {
+    public: { http: ['https://testrpc.x1.tech'] },
+    default: { http: ['https://testrpc.x1.tech'] },
+  },
+  blockExplorers: {
+    etherscan: { name: 'SnowTrace', url: 'https://www.oklink.com/x1-test' },
+    default: { name: 'SnowTrace', url: 'https://www.oklink.com/x1-test' },
+  },
+} 
+
 const { chains, publicClient } = configureChains(
-  [polygonMumbai],
+  [polygonMumbai, 
+  // arbitrumGoerli, 
+  {
+    ...filecoinCalibration,
+    iconUrl: 'https://imgs.search.brave.com/z7Q4liNUN62zLO3k11oOtRvFEPgBlueAr8IQrbt0sjU/rs:fit:40:40:1/g:ce/aHR0cHM6Ly9hc3Nl/dHMuY29pbmdlY2tv/LmNvbS9jb2lucy9p/bWFnZXMvMTI4MTcv/bGFyZ2UvZmlsZWNv/aW4ucG5nPzE2OTY1/MTI2MDk'
+  }, 
+  {
+    ...scrollSepolia,
+    iconUrl: 'https://app.nfts2me.com/assets/chains/scrollv2.svg',
+  }, 
+  {
+    ...celoAlfajores,
+    iconUrl: 'https://imgs.search.brave.com/Oi5N3WOCeTxMjR6bvBH0WYp2GNNAKVU1XSBsrShuT1o/rs:fit:40:40:1/g:ce/aHR0cHM6Ly9hc3Nl/dHMuY29pbmdlY2tv/LmNvbS9jb2lucy9p/bWFnZXMvMTEwOTAv/bGFyZ2UvSW5qWEJO/eDlfNDAweDQwMC5q/cGc_MTY5NjUxMTAz/MQ',
+  },
+  // baseGoerli, 
+  {
+    ...mantleTestnet, 
+    iconUrl: 'https://miro.medium.com/v2/0*w-6d4VpYha0olTgb.jpg',
+  },
+  {
+    ...polygonZkEvmTestnet,
+    iconUrl: 'https://zkevm.polygonscan.com/images/svg/brands/mainbrand-1.svg?v=23.10.2.0',
+  },
+  {
+    ...okxX1,
+    iconUrl:'https://static.oklink.com/cdn/assets/imgs/233/7E3D44D659CE5A9C.png?x-oss-process=image/format,webp/resize,w_88,h_88,type_6/ignore-error,1',
+  }
+],
   [
     publicProvider()
   ]
--- interface/src/constants.js
+++ interface/src/constants.js
@@ -15,4 +15,88 @@ export const NFT_CONTRACTS = {
             contractAddress: "0x075904AE760B16E62430762DfAF58F590755cfE0"
         }
     },
+    "534351": {
+        "rpc": "https://scroll-sepolia.blockpi.network/v1/rpc/public",
+        "fluidAddress": "0xD4441eCbC073970c86a6C121c47f371D1d109ceA",
+        "fluidity": "0x17e695ca08daba6f9b351008a2dd9d65cb8f679b",
+        "BAYC": {
+            contractAddress: "0x1367082758BCaAeE041992d56B53CbAdEB477079"
+        },
+        "WOW": {
+            contractAddress: "0x77325bF80225B7FDc1D7AdF3451db5A04A0973BB"
+        },
+        "AZUKI": {
+            contractAddress: "0x955B4c00E2085E00dbfC78E7FfE2c588C570bBdA"
+        }
+    },
+    "44787": {
+        "rpc": "https://alfajores-forno.celo-testnet.org",
+        "fluidAddress": "0x5055E3568Ff1bF16369BB5657977fFffBE5327f5",
+        "fluidity": "0x1ceb42fc604c8607d844Acbe2ab81BA3E3c80EAe",
+        "BAYC": {
+            contractAddress: "0x1e1E9A6E7964fe8850E475cF5e369e6316c853A6"
+        },
+        "WOW": {
+            contractAddress: "0x00ab6453080727D8ea658fF13E75becaB8BaecaF"
+        },
+        "AZUKI": {
+            contractAddress: "0x4BB8A71F7A1C5179F53fBB2982F3e47D109cDC5e"
+        }
+    },
+    "1442": {
+        "rpc": "https://rpc.public.zkevm-test.net",
+        "fluidAddress": "0x8C27cd935bc8FC9C61c67124Ca2277fFBeA8B9FE",
+        "fluidity": "0x3198397Eaa1a2E8c5acDaf2fB9044B5c11D54Ad0",
+        "BAYC": {
+            contractAddress: "0x3d0Ac4B092BcaCb4040A4109F612310c02A1F001"
+        },
+        "WOW": {
+            contractAddress: "0x05492Bd25F432393dB75c9F6CfE9CA833e118cA8"
+        },
+        "AZUKI": {
+            contractAddress: "0x817C8C7694b464AcA1332419509E0E9a4240b0EF"
+        }
+    },
+    "195": {
+        "rpc": "https://testrpc.x1.tech",
+        "fluidAddress": "0x77325bF80225B7FDc1D7AdF3451db5A04A0973BB",
+        "fluidity": "0xD4441eCbC073970c86a6C121c47f371D1d109ceA",
+        "BAYC": {
+            contractAddress: "0x955B4c00E2085E00dbfC78E7FfE2c588C570bBdA"
+        },
+        "WOW": {
+            contractAddress: "0x1367082758BCaAeE041992d56B53CbAdEB477079"
+        },
+        "AZUKI": {
+            contractAddress: "0x439BB432207B70496272a333A6D9F419156D1340"
+        }
+    },
+    "5001": {
+        "rpc": "https://rpc.testnet.mantle.xyz",
+        "fluidAddress": "0x77325bF80225B7FDc1D7AdF3451db5A04A0973BB",
+        "fluidity": "0xD4441eCbC073970c86a6C121c47f371D1d109ceA",
+        "BAYC": {
+            contractAddress: "0x955B4c00E2085E00dbfC78E7FfE2c588C570bBdA"
+        },
+        "WOW": {
+            contractAddress: "0x1367082758BCaAeE041992d56B53CbAdEB477079"
+        },
+        "AZUKI": {
+            contractAddress: "0x439BB432207B70496272a333A6D9F419156D1340"
+        }
+    },
+    "314159": {
+        "rpc": "https://api.calibration.node.glif.io/rpc/v1",
+        "fluidAddress": "0x17E695CA08DaBa6f9b351008A2DD9D65cb8F679b",
+        "fluidity": "0x52b5B8ff61CFaE415B77C0f92cd78bBD19170DA1",
+        "BAYC": {
+            contractAddress: "0x77325bF80225B7FDc1D7AdF3451db5A04A0973BB"
+        },
+        "WOW": {
+            contractAddress: "0xD4441eCbC073970c86a6C121c47f371D1d109ceA"
+        },
+        "AZUKI": {
+            contractAddress: "0x1367082758BCaAeE041992d56B53CbAdEB477079"
+        }
+    },
 }
